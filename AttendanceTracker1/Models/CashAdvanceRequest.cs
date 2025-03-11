3d0ba4b15139d9b67401003676a84a1829961a76
Jan Chester <janchesterperen@gmail.com>
2025-03-11 14:25:20 +0800

    asdfasdf

--- AttendanceTracker1/Models/CashAdvanceRequest.cs
+++ AttendanceTracker1/Models/CashAdvanceRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AttendanceTracker1.Models
 {
@@ -6,5 +7,41 @@ namespace AttendanceTracker1.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; } 
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Amount { get; set; } 
+
+        [Required]
+        public CashAdvanceRequestStatus Status { get; set; } = CashAdvanceRequestStatus.Pending; // pending, approved, rejected
+
+        [Required]
+        public DateTime RequestDate { get; set; } = DateTime.Now; // Date of request
+
+        [Required]
+        public string Reason { get; set; }
+
+        public int? ReviewedBy { get; set; }
+
+        [ForeignKey("ReviewedBy")]
+        public virtual User? Approver { get; set; }
+
+        public string? RejectionReason { get; set; }
+
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; } // Relationship with the User model
+    }
+
+    public enum CashAdvanceRequestStatus
+    {
+        Pending = 1,
+        Approved = 2,
+        Rejected = 3
     }
 }
